6ea3e8c305e6363b60fb04c372b8849f4a4773f3
yolo2themoon <markbench@163.com>
2021-11-14 23:34:51 +0800

    wip

---
+++ benchmarks/misc/end2end.py
@@ -0,0 +1,30 @@
+import os
+import time
+
+from end2end_cases import end2end_cases_list
+
+from utils import dump2json
+
+import taichi as ti
+
+
+class EndToEnd:
+    suite_name = 'end2end'
+    supported_archs = [ti.x64, ti.cuda]
+    test_cases = end2end_cases_list
+
+    def __init__(self, arch):
+        self._arch = arch
+        self._resualts = {}
+
+    def run(self):
+        print(end2end_cases_list)
+        for case in self.test_cases:
+            print(case.__name__)
+            self._resualts[case.__name__] = case(self._arch)
+
+
+if __name__ == '__main__':
+    impl = EndToEnd(ti.cuda)
+    impl.run()
+    print(dump2json(impl._resualts))
---
+++ benchmarks/misc/end2end_cases/__init__.py
@@ -0,0 +1,5 @@
+from .rasterizer import e2e_rasterizer
+from .cornell_box import e2e_cornellbox
+from .mpm99 import e2e_mpm99
+
+end2end_cases_list = [e2e_mpm99, e2e_rasterizer, e2e_cornellbox]
