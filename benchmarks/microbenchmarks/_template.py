6a6c4b7e6b3cce481b176815667309ee3b8256d7
yolo2themoon <markbench@163.com>
2022-01-11 13:21:11 +0800

    [misc] Add benchmark plan and template

--- benchmarks/microbenchmarks/_template.py
+++ benchmarks/microbenchmarks/_template.py
@@ -17,3 +17,46 @@ class BenchmarkItem:
 
     def items(self):
         return self._items.items()
+
+
+class BenchmarkPlan:
+    def __init__(self):
+        self.name = 'plan'
+        self.info = {}
+        self.plan = {}
+
+    def get_info(self):
+        return self.info
+
+    def get_plan(self):
+        return self.plan
+
+
+class BenchamrkTemplate:
+    name = 'template'
+
+    def __init__(self, arch):
+        self.arch = arch
+        self.plan = BenchmarkPlan()
+        self.results = {}
+
+    def run(self):
+        print(self.name)
+        paln_dict = self.plan.get_plan()
+
+        for plan_name, plan_dict, in paln_dict.items():
+            tag_list = plan_dict['tags']
+            print(f'{tag_list}')
+            result_dict = make_result_dict(tag_list)
+            ti.init(kernel_profiler=True, arch=get_ti_arch(self.arch))
+            for result_tag in plan_dict['results']:
+                _ms = plan_dict['func'](result_tag, *plan_dict['args'])
+                if _ms is not None:
+                    print(f'    {result_tag}={_ms}')
+                    add_result(result_dict, result_tag, _ms)
+            if len(result_dict['results']) > 0:
+                self.results[plan_name] = result_dict
+            ti.reset()
+
+        rdict = {'results': self.results, 'info': self.plan.get_info()}
+        return rdict
--- benchmarks/microbenchmarks/_utils.py
+++ benchmarks/microbenchmarks/_utils.py
@@ -1,3 +1,4 @@
+import copy
 import time
 
 import taichi as ti
@@ -19,6 +20,18 @@ class End2EndTimer:
         return ret
 
 
+def get_ti_arch(arch: str):
+    arch_dict = {
+        'cuda': ti.cuda,
+        'vulkan': ti.vulkan,
+        'opengl': ti.opengl,
+        'x64': ti.x64,
+        'cc': ti.cc,
+        'metal': ti.metal
+    }
+    return arch_dict[arch]
+
+
 def _size2tag(size_in_byte):
     size_subsection = [(0.0, 'B'), (1024.0, 'KB'), (1048576.0, 'MB'),
                        (1073741824.0, 'GB'),
@@ -26,3 +39,11 @@ def _size2tag(size_in_byte):
     for dsize, unit in reversed(size_subsection):
         if size_in_byte >= dsize:
             return str(int(size_in_byte / dsize)) + unit
+
+
+def make_result_dict(tag_list):
+    return {'tags': copy.deepcopy(tag_list), 'attributes': {}, 'results': {}}
+
+
+def add_result(result_dict, name, result):
+    result_dict['results'][name] = copy.deepcopy(result)
